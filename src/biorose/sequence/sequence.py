6a8135b3c73ca695daff763d70f703101b42ea53
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-08 21:47:02 +0100

    Convert sequence to abstract base class

--- src/biorose/sequence/sequence.py
+++ src/biorose/sequence/sequence.py
@@ -4,13 +4,18 @@ sequence-specific must implement.
 """
 
 
+from abc import ABC
 from typing import Protocol, runtime_checkable
 
 
-class Sequence(Protocol):
+class Sequence(ABC):
+    def __init__(self, seq: str, id: str = None) -> None:
+        self._id = id
+        self_seq = seq
+
     def count_monomers(self) -> None:
         """
         This method calculates the frequency of each of the monomers that can 
         occur in the sequence.
         """
-        ...
+        raise NotImplementedError
