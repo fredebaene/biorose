7b0af139ef7e4b8119a3f972fe5a4b7f91fe0337
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-08 21:50:17 +0100

    Adjust nucleic acid class

--- src/biorose/nucleic/nucleic.py
+++ src/biorose/nucleic/nucleic.py
@@ -11,12 +11,4 @@ class NucleicAcid(Sequence):
     _MONOMERS = {"A", "C", "G", "T"}
 
     def __init__(self, seq: str, id: str = None) -> None:
-        self._id = id
-        self_seq = seq
-
-    def count_monomers(self) -> None:
-        """
-        This method calculates the frequency of each of the monomers that can 
-        occur in the sequence.
-        """
-        self._monomer_counts = {monomer: 0 for monomer in self._MONOMERS}
+        super().__init__(seq=seq, id=id)
