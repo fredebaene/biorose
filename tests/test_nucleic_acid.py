2a2a46505378d7ba79e03a24c35652a49de352d0
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-08 23:03:42 +0100

    Refactor tests

--- tests/test_nucleic_acid.py
+++ tests/test_nucleic_acid.py
@@ -1,46 +1,26 @@
 from biorose.nucleic.dna import DNA
+from pathlib import Path
 import pytest
 
 
+_DATA_DIR = Path(__file__).parent.parent / "data"
+
+
 @pytest.mark.parametrize(
-    "seq, exp_res",
+    "seq_file, exp_res",
     [
-        (
-            (
-                "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCT"
-                "GATAGCAGC"
-            ),
-            "20 12 17 21",
-        ),
-        (
-            (
-                "GGATTCCTATCACATACACCTAGTGAGTCGGCTCCAGTTCACAGTGCACACATTCGTACAT"
-                "GGGGGCTCCGCATCGCGGCAGCTATCTCGTGATGCAACCTAAAAAGAGTTTAGTAGCCCCA"
-                "AGATGTCGCCAATCTGGATAGCTCTGGGACTCAATATCTAATACCCTGGTTTCCGCCCTCG"
-                "CCAACGGGCCCCATACGCTTTACCTATCTCTTACTCAGGATTTGTCGACTCACCGGCACTC"
-                "CGGACAGATCTTAAGACACGCCCTGCAGTTATTTGGTGAGATTTGCAATGATCCAACAAGA"
-                "TGCGGTTCCTTGTTTACTGCTCATGGGACTCACAGTACTACACGCCGCGACGTTTTCGGCT"
-                "AGGGCGTTGCCGACCTCCTCTACGGTGAAGACAATCCGCGACGTCGCATCTTCGTGTTGAG"
-                "CACGATGAATTCACTTGTATGTAGTGCATGCTTTCGTGAGCATCGGTTTAGCCGCGCATCG"
-                "CAATCCTTGTCGAATGCTATTAGCCCACCAGAAACCCGCGGGCGACGCTGGTACTGGTGGC"
-                "TCGGCTGACACTGAGATCGGCCTGGTGCTAAATCTATGGTCTACCACCGTGCAGCTCTAAT"
-                "TGAGAGTGCCCTTTTCTGACTCTCCTCGTGATAGGCGTGTGTACACGGGAGAGGGACATTG"
-                "GACCAAACCACCAGGCTAGGAACTGATTTTGGAGATGGCATGCCGTCTCACCAGGGGACGC"
-                "GCTGCTCCTGGCGCCGCGAGTAGTTATTGTCTCTGGCGGGATTCTGCCGCTTAGTAACTAT"
-                "TGTGTCTCGTTCCTATAGCAGTGCCTCGGGTTACCAACGCCTGCGTCTTCGCCCCAAGCCA"
-                "AGGTCTCCGCATGGTGGTACTGGGCATGCGGTCGTGCTCCGCAAAGATTTAAGATTTCAGA"
-                "CTCGTTGGGCACTACGAATAGCGTGGGTGGGCGGCGCAGGTGATTCATGTGAAGAACCATA"
-                "TAATTTAGGTTTGCTCCTAT"
-            ),
-            "208 271 260 257",
-        )
+        ("dna_sequence_001.txt", "20 12 17 21"),
+        ("dna_sequence_002.txt", "208 271 260 257"),
     ]
 )
-def test_monomer_counting(seq, exp_res):
+def test_monomer_counting(seq_file, exp_res):
     """
     This test checks if the `.count_monomers()` method correctly calculates 
     the frequencies of each of the possible monomers in the sequence.
     """
-    na = DNA(seq)
-    na.count_monomers()
-    assert na.monomer_counts == exp_res
+    file_path = _DATA_DIR / seq_file
+    with open(file_path, "r") as f:
+        seq = f.read().strip()
+    dna = DNA(seq)
+    dna.count_monomers()
+    assert dna.monomer_counts == exp_res
