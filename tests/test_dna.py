9645c1d39009b0aa4eaaddb1a7ea794c8f659b79
fredebaene <75481859+fredebaene@users.noreply.github.com>
2024-01-08 23:33:13 +0100

    Update test_dna.py

--- tests/test_dna.py
+++ tests/test_dna.py
@@ -8,19 +8,19 @@ _DATA_DIR = Path(__file__).parent.parent / "data"
 
 
 @pytest.mark.parametrize(
-    "seq_file, exp_res",
+    "dna_seq_file, exp_res",
     [
         ("dna_sequence_001.txt", "20 12 17 21"),
         ("dna_sequence_002.txt", "208 271 260 257"),
     ]
 )
-def test_monomer_counting(seq_file, exp_res):
+def test_monomer_counting(dna_seq_file, exp_res):
     """
     This test checks if the `.count_monomers()` method correctly calculates 
     the frequencies of each of the possible monomers in the sequence.
     """
-    seq_file_path = _DATA_DIR / seq_file
-    with open(seq_file_path, "r") as f:
+    dna_seq_file_path = _DATA_DIR / dna_seq_file
+    with open(dna_seq_file_path, "r") as f:
         seq = f.read().strip()
     dna = DNA(seq)
     dna.count_monomers()
