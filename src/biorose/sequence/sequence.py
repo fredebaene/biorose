f9798aa26b72e30f5a0dc5a6271a640dd363d891
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-08 22:01:02 +0100

    Adjust sequence base class

--- src/biorose/sequence/sequence.py
+++ src/biorose/sequence/sequence.py
@@ -11,12 +11,24 @@ class Sequence(ABC):
     def __init__(self, seq: str, id: str = None) -> None:
         self._id = id
         self._seq = seq
+        self._monomer_counts = {monomer: 0 for monomer in self._MONOMERS}
+
+    @property
+    def monomer_counts(self):
+        return " ".join([str(i) for i in self._monomer_counts.values()])
     
     def count_monomers(self) -> None:
         """
         This method calculates the frequency of each of the monomers that can 
         occur in the sequence.
         """
-        self._monomer_counts = {monomer: 0 for monomer in self._MONOMERS}
         for monomer in self._seq:
             self._monomer_counts[monomer] += 1
+
+    def _initialize_monomer_counts(self) -> None:
+        """
+        This method initializes a dictionary to hold the monomer frequencies 
+        in the sequence. This dictionary starts out with all count values of 
+        zero for every monomer.
+        """
+        
