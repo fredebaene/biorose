e64dd80f8bfb0bb4c1064d7283b1b42f6e3f839f
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-08 21:49:26 +0100

    Add method to count monomers

--- src/biorose/nucleic/nucleic.py
+++ src/biorose/nucleic/nucleic.py
@@ -4,7 +4,19 @@ provides relevant methods to analyze the nucleic acide.
 """
 
 
-class NucleicAcid:
+from biorose.sequence.sequence import Sequence
+
+
+class NucleicAcid(Sequence):
+    _MONOMERS = {"A", "C", "G", "T"}
+
     def __init__(self, seq: str, id: str = None) -> None:
         self._id = id
         self_seq = seq
+
+    def count_monomers(self) -> None:
+        """
+        This method calculates the frequency of each of the monomers that can 
+        occur in the sequence.
+        """
+        self._monomer_counts = {monomer: 0 for monomer in self._MONOMERS}
