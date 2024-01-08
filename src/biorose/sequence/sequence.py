770dd4ea5303a13b2ee60d3e97facc70128520d9
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-08 21:50:10 +0100

    Adjust concrete method

--- src/biorose/sequence/sequence.py
+++ src/biorose/sequence/sequence.py
@@ -11,10 +11,12 @@ class Sequence(ABC):
     def __init__(self, seq: str, id: str = None) -> None:
         self._id = id
         self_seq = seq
-
+    
     def count_monomers(self) -> None:
         """
         This method calculates the frequency of each of the monomers that can 
         occur in the sequence.
         """
-        raise NotImplementedError
+        self._monomer_counts = {monomer: 0 for monomer in self._MONOMERS}
+        for monomer in self._seq:
+            self._monomer_counts[monomer] += 1
